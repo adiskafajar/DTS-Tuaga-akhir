d085632190e43f13c74940a21c97163594d2afbd
adiskafajar <adiskafajar87@gmail>
2020-10-15 17:56:33 +0700

    menambahkan database yang tertinggal :v

--- crud_dts_native.sql
+++ crud_dts_native.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 15 Okt 2020 pada 05.10
--- Versi server: 10.4.11-MariaDB
--- Versi PHP: 7.4.6
+-- Generation Time: Oct 15, 2020 at 12:55 PM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,75 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user`
+-- Table structure for table `bo`
+--
+
+CREATE TABLE `bo` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `bo`
+--
+
+INSERT INTO `bo` (`id`, `nama`, `username`, `password`, `email`) VALUES
+(1, 'ssasdddddddd', 'asd', 'asd', 'asd@gmail.com'),
+(3, 'wwww', 'ww', 'www', 'asdffsdf@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dosen`
+--
+
+CREATE TABLE `dosen` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `dosen`
+--
+
+INSERT INTO `dosen` (`id`, `nama`, `username`, `password`, `email`) VALUES
+(1, 'otohime', 'otohime123', 'otohime123', 'otohime@gmail.com'),
+(2, 'koro-sensei', 'koro123', 'koro123', 'koro@gmail.com'),
+(4, 'dddddddd', 'ddd', 'ddd', 'ddd@gmail.com'),
+(5, 'asddddddd', 'ddd', 'ddd', 'ddd@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `satpam`
+--
+
+CREATE TABLE `satpam` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `satpam`
+--
+
+INSERT INTO `satpam` (`id`, `nama`, `username`, `password`, `email`) VALUES
+(2, 'ssss', 'dddd', 'dddd', 'dd@email.com'),
+(3, 'rrrrr', 'rrrr', 'rrrr', 'rrrrr@rrrrrr');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -36,31 +104,72 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `nama`, `username`, `password`, `email`) VALUES
-(6, 'muhamad testing', 'testing2', '123456', 'testing2@gmail.com');
+(6, 'muhamad testing tes tes', 'testing2', '123456', 'testing2@gmail.com'),
+(7, 'bambang bang bang', 'bambang123', 'bambang123', 'bambang@gmail.com'),
+(10, 'eeesd', 'eeesd', 'eeesd', 'eee@gmail.com'),
+(11, 'asddss', 'asdd', 'asdd', 'asddd@gmail.com'),
+(12, 'asd', 'asd', 'asd', 'asd@gmail.com'),
+(13, 'ssssss', 'ssssss', 'sssss', 'ssss@ssss');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `user`
+-- Indexes for table `bo`
+--
+ALTER TABLE `bo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `dosen`
+--
+ALTER TABLE `dosen`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `satpam`
+--
+ALTER TABLE `satpam`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `user`
+-- AUTO_INCREMENT for table `bo`
+--
+ALTER TABLE `bo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `dosen`
+--
+ALTER TABLE `dosen`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `satpam`
+--
+ALTER TABLE `satpam`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
